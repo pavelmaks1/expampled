5407173e79c510c67e7f561b16feed010c42bbef
pavelmaks1 <pavel.maks7@yandex.ru>
2017-04-27 20:46:10 +0200

    Finished layouts and routes

--- Gemfile
+++ Gemfile
@@ -12,10 +12,11 @@ gem 'slim-rails'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
-
+gem 'twitter-bootstrap-rails'
 
 group :development, :test do
   gem 'byebug', platform: :mri
+  gem 'guard-rspec', require: false
   gem 'rspec-rails'
   gem 'capybara'
   gem 'factory_girl_rails'
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "UserPages", type: :request do
+
+  describe 'User pages' do
+    subject { page }
+
+    describe 'signup page' do
+      before { visit signup_path }
+
+      it { should have_content('Sign Up') }
+      it { should have_title(full_title('Sign up')) }
+    end
+  end
+
+end
